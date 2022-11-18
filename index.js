98890cc4fb94aaf271f265bd3887a0b86e3edf98
alaminhossain171 <alamin.cse.171@gmail.com>
2022-11-18 21:33:35 +0600

    redux thunk done

--- index.js
+++ index.js
@@ -1,60 +1,103 @@
 const { createStore, applyMiddleware } = require("redux");
 const { default: logger } = require("redux-logger");
+const { default: thunk } = require("redux-thunk");
+
+
+const API='https://jsonplaceholder.typicode.com/todos';
 
 
 // =================>>> constains <<<========================
-const ADD_PRODUCT = 'ADD_PRODUCT';
-const GET_PRODUCT = 'GET_PRODUCT';
 
+const GET_TODOS = 'GET_TODOS';
+const SUCCESS = 'SUCCESS';
+const FAILED='FAILED'
 
-
-// =================>>> actions <<<========================
-const initialState = {
-    count: 2,
-    product: ['pen', 'watch']
+//state
+const initialState={
+    data:[],
+    loading:false,
+    error:null,
 }
 
 // =================>>> actions <<<========================
-const addProductAction = (productName) => {
-    return {
-        type: ADD_PRODUCT,
-        payload: productName
+const getTodoAction=()=>{
+    return{
+        type:GET_TODOS
     }
 }
-
-const getProductAction = () => {
-    return {
-        type: GET_PRODUCT,
-
+const successTodoAction=(data)=>{
+    return{
+        type:SUCCESS,
+        payload:data
     }
 }
-
+const faildTodoAction=(errMsg)=>{
+    return{
+        type:FAILED,
+        payload:errMsg
+    }  
+}
 
 
 // =================>>> reducer <<<========================
 
-const productReducer = (state = initialState, action) => {
+const todoReducer = (state = initialState, action) => {
     switch (action.type) {
-        case GET_PRODUCT:
-            return { ...state };
+        case GET_TODOS:
+           return{
+            ...state,
+            loading:true
+           }
 
-        case ADD_PRODUCT:
+        case SUCCESS:
             return {
-                count: state.count + 1,
-                product: [...state.product, action.payload]
+                ...state,
+                data: action.payload,
+               loading:false
+            }
+            case FAILED: 
+             return {
+                ...state,
+            loading:false,
+             error:action.payload
             }
             default:
                 return state
     }
 }
 
+const fetchData=()=>{
+
+return (dispatch)=>{
+dispatch(getTodoAction());
+
+    fetch(API)
+    .then((response) =>response.json())
+    .then((jsonResponse) => {
+    const title=jsonResponse.map((item)=>item.title);
+   dispatch(successTodoAction(title))
+
+ 
+    }).catch((error) => {
+      // Handle the error
+     dispatch(faildTodoAction(error.message))
+    });
+}
+
+
+   
+}
+
+
+
+
 //store (getState, dispatch, subscripe)
 
-const store = createStore(productReducer,applyMiddleware(logger));
+const store = createStore(todoReducer,applyMiddleware(thunk));
 
 
 store.subscribe(() => {
     console.log(store.getState());
 })
 
-store.dispatch(addProductAction('book'));
+store.dispatch(fetchData());
